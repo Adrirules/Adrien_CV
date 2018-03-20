6f99d4399af972a0a5d819176c688ee87ee0c690
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-03-20 19:08:13 +0100

    add a title to the banner and some js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import "../components/smooth_scroll";
 
-
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
