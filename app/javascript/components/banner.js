55e01ee69f824750a674f106e503be6a901dc6bb
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-03-21 18:09:23 +0100

    add Animation On Scroll, to the profil pic and content. Add a scroll down picto

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -9,3 +9,11 @@ function loadDynamicBannerText() {
 }
 
 export { loadDynamicBannerText };
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,12 @@
 import "bootstrap";
 import "../components/smooth_scroll";
+import "../components/banner";
+
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
+
+AOS.init();
