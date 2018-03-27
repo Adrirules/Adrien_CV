b9821b388ff930677f91db8547d642c765d33d4e
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-03-27 15:19:22 +0200

    adding drift bot

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,16 @@
 import "bootstrap";
 import "../components/smooth_scroll";
 import "../components/banner";
+import "../components/driftbot";
 
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 
 import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
+
+import { drift } from '../components/driftbot';
 loadDynamicBannerText();
 
 AOS.init({
