a940383021c55dc9fa59b842b61a4516d3134f78
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-03-27 15:30:11 +0200

    refacto js to allow typed.js to work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,17 @@
 import "bootstrap";
+
 import "../components/smooth_scroll";
-import "../components/banner";
-import "../components/driftbot";
-
-
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
 
+import "../components/banner";
 import { loadDynamicBannerText } from '../components/banner';
-loadDynamicBannerText();
-
-import { drift } from '../components/driftbot';
 loadDynamicBannerText();
 
 AOS.init({
   disable: 'mobile'
 });
+
+import "../components/driftbot";
+import { drift } from '../components/driftbot';
+loadDynamicBannerText();
