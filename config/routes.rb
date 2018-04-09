84b20b52ab1e55c356636aab64682b37455fd29e
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-04-09 12:54:28 +0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,7 @@ Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 
-  # match '/contacts',     to: 'contacts#new',             via: 'get'
-  # resources "contacts", only: [:new, :create]
-
-  resources :contacts, only: [:new, :create]
-
-
+  match '/contacts',     to: 'contacts#new',             via: 'get'
+  resources "contacts", only: [:new, :create]
 end
 
