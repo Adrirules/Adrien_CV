2b33d2b169ac72bccd8e3236d1ac67c6a75370df
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-04-09 13:18:05 +0200

    new routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
 
 
   scope "(:locale)", locale: /fr|en/ do
+    ressources :contacts
     resources :application
     root to: 'pages#home'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # match '/contacts',     to: 'contacts#new',             via: 'get'
+  # resources "contacts", only: [:new, :create]
   end
 
-  match '/contacts',     to: 'contacts#new',             via: 'get'
-  resources "contacts", only: [:new, :create]
 end
 
