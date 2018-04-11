2395f9a074c488d729f52a382c28322d8e5d52f3
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-04-11 13:04:44 +0200

    config meta and OG

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
   def default_url_options
   { locale: I18n.locale }
   end
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
 end
